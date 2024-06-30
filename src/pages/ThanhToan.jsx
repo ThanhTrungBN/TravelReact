738cfdc3a869ee643f439abd70e1e291d8826bbd
ThanhTrungBN <thanhtrung26012003@gmail.com>
2024-06-30 22:49:30 +0700

    BaiTapLon

--- src/pages/ThanhToan.jsx
+++ src/pages/ThanhToan.jsx
@@ -1,12 +1,7 @@
 import React, { useEffect, useState,useContext } from "react";
 import Header from '../components/header.jsx'
 import Footer from '../components/footer.jsx';
-import Home from '../layout/home.jsx';
-import Pay from "../layout/pay.jsx";
-import { TravelMenu } from "../constant/context.js";
-import { DiadiemMB, DiadiemMT, DiadiemMTNB, DiadiemMDNB } from '../services/location.services.js'
-import { loadNguoidungbyID ,AuthContext} from '../services/login.services.js'
-import PayTest from "../layout/paytest.jsx";
+import PayTest from "../layout/vnpay.jsx";
 const Thanhtoan = () => {
   useEffect(() => {
     document.title = "Pay";
